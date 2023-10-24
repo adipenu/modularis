e4747b48ea7a2062e1fcb2211620526be9f7d8dc
JanicaD <74816999+JanicaD@users.noreply.github.com>
2023-10-24 12:45:42 -0400

    Update setup.py to include launch file

--- thruster_pwm/pwmdriver/setup.py
+++ thruster_pwm/pwmdriver/setup.py
@@ -11,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')), # This line for launch files
     ],
     install_requires=['setuptools'],
     zip_safe=True,
